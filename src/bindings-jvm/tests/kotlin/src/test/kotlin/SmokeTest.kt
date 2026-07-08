7ca33828a59a48319e3d5f7d820d80647f3b71dd
Satyaki Ghosh <satyakig@amazon.com>
2026-07-08 01:28:56 -0400

    Fix kotlin test assert

--- src/bindings-jvm/tests/kotlin/src/test/kotlin/SmokeTest.kt
+++ src/bindings-jvm/tests/kotlin/src/test/kotlin/SmokeTest.kt
@@ -436,7 +436,7 @@ class SmokeTest {
 
     @Test
     fun rulesEvaluatedIsFullRuleCount() {
-        val expected = 280
+        val expected = 280u
         assertEquals(expected, CEL.validateDetailed(templateFile("good/generic.yaml"), defaultConfig()).metadata.rulesEvaluated, "cel: rulesEvaluated")
         assertEquals(expected, REGO.validateDetailed(templateFile("good/generic.yaml"), defaultConfig()).metadata.rulesEvaluated, "rego: rulesEvaluated")
     }
